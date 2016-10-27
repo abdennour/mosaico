cd0d06301eba2e891cc15d2090d63c80ebf842f0
abdennour <mail@abdennoor.com>
2016-10-27 07:22:35 +0300

    beautify backend facade

--- backend/main.js
+++ backend/main.js
@@ -1,12 +1,8 @@
 "use strict";
 /* global module: false, console: false, __dirname: false */
-
 const express = require('express');
 const upload = require('jquery-file-upload-middleware');
 const bodyParser = require('body-parser');
-
-
-
 const config = require('../server-config.js');
 const app = express();
 const routing = require('./routing');
@@ -14,22 +10,15 @@ const services = require('./services');
 
 app.use(require('connect-livereload')({ ignore: [/^\/dl/] }));
 // app.use(require('morgan')('dev'));
-
 app.use(bodyParser.json({limit: '5mb'}));
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   limit: '5mb',
   extended: true
 }));
+app.use('/upload/', upload.fileHandler(services.UploadService.uploadOptions));
 
 app.post('/dl/', routing('post'));
 app.get('/upload/', routing('get'));
 app.get('/img/', routing('get'));
-
-//---------------------
-
-
-
-app.use('/upload/', upload.fileHandler(services.UploadService.uploadOptions));
-
 
 module.exports = app;
